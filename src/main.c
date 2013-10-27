c05e6e6fced13b65c778262ec95f37155a82d6c1
Paul Doerr <doerrpau@umich.edu>
2013-10-27 15:28:12 -0400

    Added OpenCV image display template and example image

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "cv.h"
+#include "highgui.h"
+ 
+int main(int argc, char** argv)
+{
+    IplImage* img = cvLoadImage( "../images/dog.jpg" );
+    cvNamedWindow( "Example1", CV_WINDOW_AUTOSIZE );
+    cvShowImage("Example1", img);
+    cvWaitKey(0);
+    cvReleaseImage( &img );
+    cvDestroyWindow( "Example1" );
+    return 0;
+}

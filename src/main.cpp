6b0b27f4ffbda247778cf4696dd284d52d0d1ca4
adutta <adutta13@gmail.com>
2013-10-31 15:36:56 -0400

    Added mouse funcitonality to alter size of grabCut.

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,87 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
+
+bool drawing_box;
+cv::Rect box;
+
+void callMouse(int event,int x,int y,int flags,void* param)
+{
+
+    switch( event )
+    {
+        case CV_EVENT_LBUTTONDOWN:
+        {
+            drawing_box=true;
+            box = cvRect( x, y, 0, 0 );
+        }
+            break;
+        case CV_EVENT_MOUSEMOVE:
+        {
+            if( drawing_box )
+            {
+                box.width = x-box.x;
+                box.height = y-box.y;
+            }
+            break;
+        }
+        case CV_EVENT_LBUTTONUP:
+        {   
+            drawing_box=false;
+            if( box.width < 0 )
+            {
+                box.x += box.width;
+                box.width *= -1;
+            }
+
+            if( box.height < 0 )
+            {
+                box.y += box.height;
+                box.height *= -1;
+            }
+
+            // Cast input image from void* to Mat*
+            cv::Mat* image  = static_cast<cv::Mat *>(param);
+
+            // Draw & show
+            rectangle(*image, box,Scalar(255,255,255),1);
+            imshow("Dog",*image);
+            
+            break;
+        }
+        default:
+            break;
+    }
+}
 
 int main(int argc, char** argv)
 {
     /* Load the image */
     Mat image = imread( "../images/dog.jpg" );
 
+    namedWindow("Dog");
+    imshow("Dog",image);
+
     // define bounding rectangle
     int border = 75;
     int border2 = border + border;
     cv::Rect rectangle(border,border,image.cols-border2,image.rows-border2);
- 
+    
+    setMouseCallback("Dog",callMouse,&image);
+
     Mat result; // segmentation result (4 possible values)
     Mat bgModel,fgModel; // the models (internally used)
  
+    waitKey();
+
     // Perform GrabCut segmentation
     grabCut(image,    // input image
         result,   // segmentation result
-        rectangle,// rectangle containing foreground 
+        box/*rectangle*/,// rectangle containing foreground 
         bgModel,fgModel, // models
         1,        // number of iterations
         GC_INIT_WITH_RECT); // use rectangle
@@ -32,10 +92,9 @@ int main(int argc, char** argv)
     image.copyTo(foreground,result); // bg pixels not copied
  
     // draw rectangle on original image
-    cv::rectangle(image, rectangle, Scalar(255,255,255),1);
-    namedWindow("Dog");
+    //cv::rectangle(image, rectangle, Scalar(255,255,255),1);
     imshow("Dog",image);
- 
+
     // display result
     namedWindow("Segmented Dog");
     imshow("Segmented Dog",foreground);

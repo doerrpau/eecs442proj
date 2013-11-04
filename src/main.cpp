d1e371983bff3f32a932c6d38a9bb6c439d05d6b
adutta <adutta13@gmail.com>
2013-11-04 12:43:46 -0500

    Created function to create text boxes.

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,32 @@ bool drawing_box;
 cv::Rect box;
 Mat orig;
 
+void drawTextBox(Mat img, String text, Scalar bgColor,Scalar fgColor, Point coords)
+{
+    // Draw a text box on image
+    // Note about locations: putText draws from lower left corner, while rectangle does verticies
+    // Note about color: it's BGR, not RGB
+    rectangle(img, coords,Point(coords.x+text.length()*19.5,coords.y+50), bgColor, -1, 8, 0);
+    putText(img, text, Point(coords.x+5,coords.y+25), FONT_HERSHEY_TRIPLEX, 1, fgColor,2, 8,false);
+}
+
 void callMouse(int event,int x,int y,int flags,void* param)
 {
 
     switch( event )
     {
+        // Event handling
         case CV_EVENT_LBUTTONDOWN:
         {
+            // Start drawing box
             drawing_box=true;
             box = cvRect( x, y, 0, 0 );
-        }
             break;
+        }
+
         case CV_EVENT_MOUSEMOVE:
         {
+            // Alter box box params
             if( drawing_box )
             {
                 box.width = x-box.x;
@@ -32,6 +45,7 @@ void callMouse(int event,int x,int y,int flags,void* param)
         }
         case CV_EVENT_LBUTTONUP:
         {   
+            // Finish drawing box
             drawing_box=false;
             if( box.width < 0 )
             {
@@ -52,6 +66,7 @@ void callMouse(int event,int x,int y,int flags,void* param)
             // Draw & show
             rectangle(*image, box,Scalar(255,255,255),1);
 
+            // Reshow image
             imshow("Dog",*image);
 
             Mat result; // segmentation result (4 possible values)
@@ -64,12 +79,17 @@ void callMouse(int event,int x,int y,int flags,void* param)
                 bgModel,fgModel, // models
                 1,        // number of iterations
                 GC_INIT_WITH_RECT); // use rectangle
+
             // Get the pixels marked as likely foreground
             compare(result,GC_PR_FGD,result,CMP_EQ);
+
             // Generate output image
             Mat foreground(image->size(),CV_8UC3,Scalar(255,255,255));
             image->copyTo(foreground,result); // bg pixels not copied
-         
+
+            // Adding text above segmentation
+            drawTextBox(foreground, "I CAN HAZ FRIZBEE?", Scalar(000,000,000),Scalar(255,255,255), Point(box.x, box.height));
+
             // display result
             namedWindow("Segmented Dog");
             imshow("Segmented Dog",foreground);
@@ -79,6 +99,8 @@ void callMouse(int event,int x,int y,int flags,void* param)
             break;
         }
         default:
+            drawing_box=false;
+            box = cvRect( x, y, 0, 0 );
             break;
     }
 }
